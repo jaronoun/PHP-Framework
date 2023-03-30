4531e60fa77f0b40cd0c1be8353ae5d60174aa0a
Dorienk97 <dorienkuperus@gmail.com>
2023-03-30 14:57:45 +0200

    bootstrap fix

--- app/Views/users/index.php
+++ app/Views/users/index.php
@@ -1,21 +1,24 @@
 <?php
 // Include the necessary files for the view
-require __DIR__.'/../../../vendor/autoload.php';
+require_once __DIR__.'/../../../vendor/autoload.php';
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Home Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-bwGRhYgEKu+l7VrlFlE3Mq7pGmi+jtcBmGNVZjzTPhT+CJBBEzOHbK7p/1zTCX9N" crossorigin="anonymous" media="screen" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <!-- JavaScript en popper.js (nodig voor sommige Bootstrap-functies) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-78S+1JpO1M+O4ovMv4Qo4sZLx1/35lWNb/TrA2rCmFKy/lXzNcL6GStfntChuwN1" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js" integrity="sha512-BqC0nl1vAvOcGah+H5I8PffW6UNvwn6l5B5tkxU8zyK52YrLwQrweoOyMnzO9zGvJvzH+I0b1Ox+GWRNn/mcrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
 
 </head>
 <body>
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">My App</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -49,14 +52,8 @@ require __DIR__.'/../../../vendor/autoload.php';
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper-base.min.js"
-        integrity="sha256-Ps9zKxSGeJZgYYklwY1F/jfzG2jIVTJmXLDFeKAEfVE=" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOUXw704+6UzI6Gw0W1qbI1woDvL8o/gr/a+M9nXsh1XHp"
-        crossorigin="anonymous"></script>
+<script src="//code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
 
