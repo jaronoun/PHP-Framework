af068da961805ef62d3856649dc5f70bdda8457e
Dorienk97 <dorienkuperus@gmail.com>
2023-03-30 14:36:24 +0200

    bootstrap fix

--- app/Views/users/index.php
+++ app/Views/users/index.php
@@ -1,13 +1,19 @@
 <?php
 // Include the necessary files for the view
+require __DIR__.'/../../../vendor/autoload.php';
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Home Page</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-          integrity="sha384-OgVRvuATP1z7JjHLkuOUXw704+6UzI6Gw0W1qbI1woDvL8o/gr/a+M9nXsh1XHp" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-bwGRhYgEKu+l7VrlFlE3Mq7pGmi+jtcBmGNVZjzTPhT+CJBBEzOHbK7p/1zTCX9N" crossorigin="anonymous" media="screen" >
+
+    <!-- JavaScript en popper.js (nodig voor sommige Bootstrap-functies) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-78S+1JpO1M+O4ovMv4Qo4sZLx1/35lWNb/TrA2rCmFKy/lXzNcL6GStfntChuwN1" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js" integrity="sha512-BqC0nl1vAvOcGah+H5I8PffW6UNvwn6l5B5tkxU8zyK52YrLwQrweoOyMnzO9zGvJvzH+I0b1Ox+GWRNn/mcrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -54,7 +60,7 @@
 </body>
 </html>
 
-
 <?php
+// Include the necessary files for the view
 
 ?>
