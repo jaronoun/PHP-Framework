abc192e9357fa30740d58a59739ae322cb1056d4
Dorienk97 <dorienkuperus@gmail.com>
2023-04-13 13:35:53 +0200

    update

---
+++ app/views/layout/main.js
@@ -0,0 +1,8 @@
+<script>
+    const sidebarToggle = document.getElementById('sidebarToggle');
+    const sidebar = document.getElementById('sidebar');
+
+    sidebarToggle.addEventListener('click', function() {
+    sidebar.classList.toggle('active');
+});
+</script>
