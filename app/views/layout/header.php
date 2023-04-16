e429a55f628a286ce4fe8c7ea60fdb515b44b514
jaronoun <58861137+jaronoun@users.noreply.github.com>
2023-04-16 17:19:16 +0200

    header and footer

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -16,9 +16,6 @@ require __DIR__.'/../../../vendor/autoload.php';
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
         <a class="navbar-brand">Isoros</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav me-auto">
                     <?php if ($loggedIn): ?>
