0925eec89edef1b34bef84d603839deaa33ceb67
jaronoun <58861137+jaronoun@users.noreply.github.com>
2023-04-16 16:47:56 +0200

    Revert "boem"
    This reverts commit 2c91f67caac8cf3a7b94f16a24abbef4760060a8.

---
+++ app/views/grades/index.php
@@ -0,0 +1,66 @@
+<?php?>
+<div class="container">
+        <div class="row mt-3">
+            <!-- Most recent result card -->
+            <div class="col-12 col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-header">Meest recente resultaat</div>
+                    <div class="card-body">
+                        <h1 class="display-1 text-center">85</h1>
+                        <h2 class="text-center mb-4">Wiskunde</h2>
+                        <p class="text-center mb-0">Examendatum: 16 april 2023</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Completed exams card -->
+            <div class="col-12 col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-header">Opkomende examens</div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <h5>Wiskunde</h5>
+                                <p>10 mei 2023</p>
+                                <p>Tijd: 10:00 - 12:00</p>
+                            </li>
+                            <li class="list-group-item">
+                                <h5>Nederlands</h5>
+                                <p>15 mei 2023</p>
+                                <p>Tijd: 14:00 - 16:00</p>
+                            </li>
+                            <li class="list-group-item">
+                                <h5>Geschiedenis</h5>
+                                <p>20 mei 2023</p>
+                                <p>Tijd: 9:00 - 11:00</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- Uncompleted exams card -->
+            <div class="col-12 col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-header">Behaalde examens</div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <h5 class="mb-1">Wiskunde</h5>
+                                <p class="mb-1">Behaald met een 8.5</p>
+                                <small>2 maart 2023</small>
+                            </li>
+                            <li class="list-group-item">
+                                <h5 class="mb-1">Nederlands</h5>
+                                <p class="mb-1">Behaald met een 7.8</p>
+                                <small>14 februari 2023</small>
+                            </li>
+                            <li class="list-group-item">
+                                <h5 class="mb-1">Engels</h5>
+                                <p class="mb-1">Behaald met een 9.0</p>
+                                <small>18 januari 2023</small>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
