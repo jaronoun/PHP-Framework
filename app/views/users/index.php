e5786c0efb159b98519903135c16a1a1e758a42f
jaronoun <58861137+jaronoun@users.noreply.github.com>
2023-06-21 19:47:16 +0200

    Update index.php

--- app/views/users/index.php
+++ app/views/users/index.php
@@ -3,6 +3,7 @@
 <div class="row">
     <div class="col-12">
         <div class="card">
+            <div class="card-header text-white bg-dark">Profiel</div>
             <div class="card-body">
                 <h5 class="card-title">Name: {{ name }}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Email: {{ email }} </h6>
